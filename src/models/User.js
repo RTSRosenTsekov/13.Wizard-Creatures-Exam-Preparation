1d5946ecaf9ab7deef8eea0d67b29a830f19e753
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-18 17:55:14 +0300

    validate password missmatch

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName:{type:String, require: true},
-    lastName:{type:String , require: true},
-    email:{type: String,require: true , unique:true},
-    password: {type:String , require: true},
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
 });
 
-const User = mongoose.model('User', userSchema);
+// Проверяваме дали паролата и повторената парола са еднакви
+userSchema.virtual("repeatePassword").set(function name(value) {
+  console.log({ value });
+  if(value!==this.password){
+    throw new Error("Password missmatch!");
+    
+  }
+});
 
-module.exports=User;
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
