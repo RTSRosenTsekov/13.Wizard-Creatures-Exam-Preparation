4b1b14c35d2949743d8f05abbd6da33692c8ecf8
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-18 14:56:33 +0300

    Config view engine, add main layout ,fix hiperlink styless

--- instructions.md
+++ instructions.md
@@ -1,3 +1,4 @@
 1. Init project and structure  " npm init -y "
 2. npm i express
 3. npm i nodemon -D
+4. npm i express-handlebars
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const path = require("path");
 const app = express();
 const {PORT} = require("./constants");
+const routes = require("./router");
 
+// -- Express configuration
 // Да зареди CSS 
 app.use(express.static(path.resolve(__dirname, "./public")));
 // Да може да четем данни от body
 app.use(express.urlencoded({extended:false}));
+//--
+
+// -- Handlebarse configuration
+app.engine('hbs', handlebars.engine({extname: "hbs"}));
+app.set("view engine" , "hbs");
+app.set("views", "src/views");
+//--
+
 
 app.get('/', (req,res)=>{
-    res.send("Hello home page!")
+    res.render('home')
 });
+
+
+app.use(routes);
 
 app.listen(PORT, ()=> console.log(`Server is listening on port: ${PORT}`));
