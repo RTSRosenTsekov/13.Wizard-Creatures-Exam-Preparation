d2e6d5e7c21e2d98bdd9646adfc9548450885c0e
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-18 17:25:42 +0300

    add userControler and routes , render login , register pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
+const userController = require("./controllers/userController");
 // TO DO add endpoints with controllers here...
 
 router.use(homeController);
+router.use('/users', userController)
+
 module.exports = router;
