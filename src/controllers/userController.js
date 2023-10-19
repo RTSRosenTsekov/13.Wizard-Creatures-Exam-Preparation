e41114843740bec85a4465c02b09a2d833ad11b6
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-19 12:23:19 +0300

    Error handling , add 404 page, add error util, show error notification on main layout for login,regi

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,35 +1,53 @@
 const router = require("express").Router();
 const userService = require("../services/userServices");
+const { extractErrorMsgs } = require("../utils/errorHandler");
 
-
-router.get('/register', (req,res)=>{
-    res.render('user/register');
+router.get("/register", (req, res) => {
+  res.render("user/register");
 });
 
-router.post('/register', async (req,res)=>{
-    const {firstName , lastName,email,password , repeatPassword}=req.body;
-    await userService.register({firstName , lastName,email,password , repeatPassword});
-    res.redirect('/users/login');
+router.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password, repeatPassword } = req.body;
+
+  try {
+    await userService.register({
+      firstName,
+      lastName,
+      email,
+      password,
+      repeatPassword,
+    });
+    
+    res.redirect("/users/login");
+  } catch (error) {
+    const errorMessage = extractErrorMsgs(error);
+
+    res.status(404).render("user/register", { errorMessage });
+  }
 });
 
-router.get('/login', (req,res)=>{
-    res.render('user/login');
+router.get("/login", (req, res) => {
+  res.render("user/login");
 });
 
-router.post('/login',async (req,res)=>{
-    const {email,password }=req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await userService.login(email, password);
 
-    const token = await userService.login(email,password);
-   
-    res.cookie("token", token,{httpOnly:true});
+    res.cookie("token", token, { httpOnly: true });
 
-    res.redirect('/')
+    res.redirect("/");
+  } catch (error) {
+    const errorMessage = extractErrorMsgs(error);
+
+    res.status(404).render("user/login", { errorMessage });
+  }
 });
 
-router.get('/logout',(req,res)=>{
-    res.clearCookie('token');
-    res.redirect('/');
-})
-
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
 
 module.exports = router;
